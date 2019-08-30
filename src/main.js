ef1b98135bc1348fc92fb312e56b954377c81c1a
zhaohai <zhaohai@hzyushi.cn>
2019-08-30 15:48:31 +0800

    + 引入element-ui

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false // 阻止 vue 在启动时生成生产提示
+
+// 引入element-ui，并全局配置
+Vue.use(ElementUI, { size: 'small' })
 
 new Vue({
     router,
--- src/views/welcome/welcome.vue
+++ src/views/welcome/welcome.vue
@@ -1,14 +1,24 @@
 <template>
     <div class="welcome">
-        aaaaaaaa
+        <el-button type="primary" @click.once="enter('hello', $event)">主要按钮</el-button>
+        <div>
+            <router-link to="/login">Go to Login</router-link>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'welcome',
-    components: {
-
+    data () {
+        return {
+        }
+    },
+    methods: {
+        enter (msg, event) {
+            console.log('msg: ', msg)
+            console.log('event: ', event)
+        }
     }
 }
 </script>
